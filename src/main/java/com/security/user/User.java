69a5988244a60a08470680dc63138c863771aabe
felipematheus1337 <lipehbr@gmail.com>
2023-01-28 14:54:49 -0300

    Implementação da Interface UserDetails na entidade User, com Override dos métodos

--- src/main/java/com/security/user/User.java
+++ src/main/java/com/security/user/User.java
@@ -37,27 +37,32 @@ public class User implements UserDetails {
     }
 
     @Override
-    public String getUsername() {
+    public String getPassword() {
         return null;
     }
 
     @Override
+    public String getUsername() {
+        return password;
+    }
+
+    @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
