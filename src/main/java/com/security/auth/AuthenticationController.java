5649469bdaac7c529ca46c60a05beb2814a73ef2
felipematheus1337 <lipehbr@gmail.com>
2023-01-30 18:51:30 -0300

    Método de registro finalizado!

--- src/main/java/com/security/auth/AuthenticationController.java
+++ src/main/java/com/security/auth/AuthenticationController.java
@@ -28,7 +28,7 @@ public class AuthenticationController {
     }
 
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> register(
+    public ResponseEntity<AuthenticationResponse> authenticate(
             @RequestBody AuthenticationRequest request) {
         return ResponseEntity.ok(authenticationService.authenticate(request));
 
--- src/main/java/com/security/auth/service/AuthenticationService.java
+++ src/main/java/com/security/auth/service/AuthenticationService.java
@@ -1,16 +1,39 @@
 package com.security.auth.service;
 
+import com.security.UserRepository;
 import com.security.auth.dto.AuthenticationRequest;
 import com.security.auth.dto.AuthenticationResponse;
 import com.security.auth.dto.RegiterRequest;
+import com.security.config.JwtService;
+import com.security.user.Role;
+import com.security.user.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
 
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+
+    private final JwtService jwtService;
+
     public AuthenticationResponse register(RegiterRequest request) {
+        var user = User.builder()
+                .firstName(request.getFirstname())
+                .lastName(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
